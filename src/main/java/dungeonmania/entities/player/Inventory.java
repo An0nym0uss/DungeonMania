dea955b95682c35d597a786f89921103d42ddbfc
Enoch <enoch7225@gmail.com>
2021-10-22 16:02:45 +1100

    Added skeleton code and fixed minor issues

--- src/main/java/dungeonmania/entities/player/Inventory.java
+++ src/main/java/dungeonmania/entities/player/Inventory.java
@@ -3,28 +3,27 @@ package dungeonmania.entities.player;
 import java.util.ArrayList;
 import java.util.List;
 
-import dungeonmania.entities.collectable.CollectableEntities;
-import dungeonmania.entities.player.Recipe;
+import dungeonmania.entities.collectable.CollectableEntity;
 
 public class Inventory {
 
-    private List<CollectableEntities> items = new ArrayList<CollectableEntities>();
+    private List<CollectableEntity> items = new ArrayList<CollectableEntity>();
     private List<Recipe> recipe = new ArrayList<Recipe>();
 
-    public void addItem(CollectableEntities item) {
+    public void addItem(CollectableEntity item) {
 
     }
 
-    public boolean checkItem(CollectableEntities item) {
+    public boolean checkItem(CollectableEntity item) {
 
         return false;
     }
 
-    public void addRecipe(CollectableEntities item) {
+    public void addRecipe(CollectableEntity item) {
 
     }
 
-    public boolean checkRecipe(CollectableEntities item) {
+    public boolean checkRecipe(CollectableEntity item) {
 
         return false;
     }
