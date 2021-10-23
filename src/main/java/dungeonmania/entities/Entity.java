8032c3d06079df83ea8b6ac3dae18dfb846e286c
Enoch <enoch7225@gmail.com>
2021-10-23 21:05:46 +1100

    Added setter and Getter for Entity

--- src/main/java/dungeonmania/entities/Entity.java
+++ src/main/java/dungeonmania/entities/Entity.java
@@ -9,6 +9,27 @@ public abstract class Entity {
     private boolean isInteractable;
 
     /**
+     * Getter for position.
+     * 
+     * @return
+     */
+    public Position getPosition() {
+        return position;
+    }
+
+    /**
+     * Setter for position
+     * 
+     * @param position
+     * 
+     * @pre 0 <= x < WIDTH
+     * @pre 0 <= y < HEIGHT
+     */
+    public void setPosition(int x, int y) {
+        position = position.translateBy(x, y);
+    }
+
+    /**
      * What happens every tick.
      */
     public void update() {
