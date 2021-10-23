7c64be8287be54a2a8547729bfa1ad871759047c
Enoch <enoch7225@gmail.com>
2021-10-23 22:37:38 +1100

    completed getJSON methods for existing classes and added some more documentation

--- src/main/java/dungeonmania/goals/AndCompositeGoal.java
+++ src/main/java/dungeonmania/goals/AndCompositeGoal.java
@@ -3,33 +3,62 @@ package dungeonmania.goals;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 /**
+ * @author Enoch Kavur (z5258204)
+ * 
  * @invarient sub goals never change.
  */
 public class AndCompositeGoal implements ComponentGoal {
 
     private List<ComponentGoal> subgoals = new ArrayList<ComponentGoal>();   
     
+    /**
+     * 
+     * @param componentGoal is subgoal to be added
+     * @pre componentGoal is not null
+     */
     public void addSubgoal(ComponentGoal componentGoal) {
         subgoals.add(componentGoal);
     }
 
+    /**
+     * 
+     * @post returns list of subgoals.
+     */
     public List<ComponentGoal> getSubgoals() {
         return subgoals;
     }
 
+    /**
+     * 
+     * @post will return true if all subgoals are achieved.
+     */
     @Override
     public boolean isAchieved() {
 
-        return !subgoals.stream().filter(goal -> !goal.isAchieved()).findAny().isPresent();
+        return subgoals.stream().filter(goal -> !goal.isAchieved()).findAny().isEmpty();
     }
 
+    /**
+     * 
+     * @post return JSON Object with an array of subgoals in the form 
+     * {"goal": "AND", "subgoals" : [...]}
+     */
     @Override
     public JSONObject getJSON() {
-        // TODO Auto-generated method stub
-        return null;
+
+        JSONObject goal = new JSONObject();
+        JSONArray subgoals = new JSONArray();
+ 
+        this.subgoals.stream().forEach(g -> subgoals.put(g.getJSON()));
+
+        goal.put("goal", "AND");
+        goal.put("subgoals", subgoals);
+
+        return goal;
     }
 
 }
--- src/main/java/dungeonmania/goals/ReachedExitGoal.java
+++ src/main/java/dungeonmania/goals/ReachedExitGoal.java
@@ -2,17 +2,36 @@ package dungeonmania.goals;
 
 import org.json.JSONObject;
 
+import dungeonmania.Grid;
+
+/**
+ * A goal for reaching the exit of a dungeon.
+ * 
+ * @author Enoch Kavur (z5258204)
+ * 
+ * @invarient grid is always a reference that current dungeon.
+ */
 public class ReachedExitGoal implements ComponentGoal {
+
+	private Grid grid;
+
+	public ReachedExitGoal(Grid grid) {
+		this.grid = grid;
+	}
 
 	@Override
 	public boolean isAchieved() {
-		// TODO Auto-generated method stub
+
 		return false;
 	}
 
 	@Override
 	public JSONObject getJSON() {
-		// TODO Auto-generated method stub
-		return null;
+
+		JSONObject goal = new JSONObject();
+		
+		goal.put("goal", "exit");
+
+		return goal;
 	}
 }
