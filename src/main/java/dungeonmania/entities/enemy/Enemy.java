00c7200c2d97bf6b0a0f3c788014733890e2530c
William Wong <z5205428@ad.unsw.edu.au>
2021-10-24 18:42:27 +1100

    fixed a typo in Moving

--- src/main/java/dungeonmania/entities/enemy/Enemy.java
+++ src/main/java/dungeonmania/entities/enemy/Enemy.java
@@ -1,7 +1,18 @@
 package dungeonmania.entities.enemy;
 
 import dungeonmania.entities.Entity;
+import dungeonmania.entities.Health;
+import dungeonmania.entities.Moving;
+import dungeonmania.entities.Damage;
 
 public abstract class Enemy extends Entity {
     private int speed;
+
+    public Enemy(int speed) {
+        this.speed = speed;
+    }
+
+
+
+
 }
