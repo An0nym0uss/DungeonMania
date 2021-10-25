673b076cd70047fcf68265a51c6bb145f543675b
Austin Chen <yizhou.chen2@student.unsw.edu.au>
2021-10-26 01:12:50 +1100

    Update StatusEffect.java

--- src/main/java/dungeonmania/entities/player/StatusEffect.java
+++ src/main/java/dungeonmania/entities/player/StatusEffect.java
@@ -1,7 +1,43 @@
 package dungeonmania.entities.player;
 
-public enum StatusEffect {
-    Invincible,
-    Invisible;
-    
+public class StatusEffect {
+
+    private boolean invincible;
+    private int invincibleDuration;
+    private boolean invisible;
+    private int invinvisibleDuration;
+
+
+    public boolean isInvincible() {
+        return this.invincible;
+    }
+
+    public void setInvincible(boolean invincible) {
+        this.invincible = invincible;
+    }
+
+    public int getInvincibleDuration() {
+        return this.invincibleDuration;
+    }
+
+    public void setInvincibleDuration(int invincibleDuration) {
+        this.invincibleDuration = invincibleDuration;
+    }
+
+    public boolean isInvisible() {
+        return this.invisible;
+    }
+
+    public void setInvisible(boolean invisible) {
+        this.invisible = invisible;
+    }
+
+    public int getInvinvisibleDuration() {
+        return this.invinvisibleDuration;
+    }
+
+    public void setInvinvisibleDuration(int invinvisibleDuration) {
+        this.invinvisibleDuration = invinvisibleDuration;
+    }
+
 }
