ee60faa6018549906e7cbc28cd04a9276d6887ed
Enoch <enoch7225@gmail.com>
2021-10-26 01:09:53 +1100

    Added abstract factory pattern for creating dungeon responses and completed some implementation of the concrete factory

---
+++ src/main/java/dungeonmania/response/models/StandardResponseFactory.java
@@ -0,0 +1,88 @@
+package dungeonmania.response.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import dungeonmania.Dungeon;
+import dungeonmania.Grid;
+import dungeonmania.entities.Entity;
+import dungeonmania.entities.collectable.CollectableEntity;
+import dungeonmania.entities.enemy.Enemy;
+import dungeonmania.entities.player.Inventory;
+import dungeonmania.util.Position;
+
+/**
+ * 
+ * 
+ * @author Enoch Kavur (z5258204)
+ */
+public class StandardResponseFactory implements ResponseFactory {
+
+    /**
+     * @param dungeon
+     * @pre dungeon is not null
+     * @post DungeonResponse is created and returned
+     */
+    @Override
+    public DungeonResponse createDungeonResponse(Dungeon dungeon) {
+        String dungeonId = dungeon.getDungeonId();
+        String dungeonName = dungeon.getDungeonName();
+        List<EntityResponse> entities = createEntityResponseList(dungeon.getGrid());
+        List<ItemResponse> inventory = new ArrayList<>();
+        //createItemResponseList(dungeon.getGrid().getPlayer().getInventory()); // Bad code right here.
+        List<String> buildables = new ArrayList<>();// TODO
+        String goals = ":exit"; // TODO
+        List<AnimationQueue> animations = new ArrayList<>();
+
+        return new DungeonResponse(dungeonId, dungeonName, entities, inventory, buildables, goals);
+    }
+
+    /**
+     * @param grid
+     * @pre grid is not null
+     * @post List of EntityResponse is created and returned
+     */
+    @Override
+    public List<EntityResponse> createEntityResponseList(Grid grid) {
+
+        List<EntityResponse> entities = new ArrayList<>();
+
+        for (int x = 0; x < grid.getWidth(); x++) {
+            for (int y = 0; y < grid.getHeight(); y++) {
+                for (Entity entity : grid.getEntities(x, y)) {
+                    entities.add(createEntityResponse(entity));
+                }
+            }
+        }
+        
+        return entities;
+    }
+
+    /**
+     * @param entity
+     * @pre entity is not null
+     * @post EntityResponse is created and returned
+     */
+    @Override
+    public EntityResponse createEntityResponse(Entity entity) {
+        String id = entity.getId();
+        String type = entity.getType();
+        Position position = entity.getPosition();
+        boolean isInteractable = entity.isInteractable();
+
+        return new EntityResponse(id, type, position, isInteractable);
+    }
+
+    @Override
+    public List<ItemResponse> createItemResponseList(Inventory inventory) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public ItemResponse createItemResponse(CollectableEntity item) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
