aa02ea4bbc087cc7ec582db50bf26fdea10ede15
Austin Chen <yizhou.chen2@student.unsw.edu.au>
2021-10-26 00:20:50 +1100

    Update InvisibilityPotion.java

--- src/main/java/dungeonmania/entities/collectable/InvisibilityPotion.java
+++ src/main/java/dungeonmania/entities/collectable/InvisibilityPotion.java
@@ -1,5 +1,16 @@
 package dungeonmania.entities.collectable;
 
-public class InvisibilityPotion {
-    
+import dungeonmania.entities.player.Player;
+
+public class InvisibilityPotion extends CollectableEntity {
+
+    public InvisibilityPotion(String id, String type) {
+        this.id = id;
+        this.type = "health_potion";
+    }
+
+    @Override
+    public void useItem(Player player) {
+        player.setInvisible(true);
+    }
 }
