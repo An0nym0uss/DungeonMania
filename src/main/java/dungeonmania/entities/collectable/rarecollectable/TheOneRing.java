671c69a61360e51bea7852b54fdf5f8295255313
Austin Chen <yizhou.chen2@student.unsw.edu.au>
2021-10-28 18:07:22 +1100

    Update TheOneRing.java

--- src/main/java/dungeonmania/entities/collectable/rarecollectable/TheOneRing.java
+++ src/main/java/dungeonmania/entities/collectable/rarecollectable/TheOneRing.java
@@ -1,5 +1,10 @@
 package dungeonmania.entities.collectable.rarecollectable;
 
-public class TheOneRing {
+public class TheOneRing extends RareCollectableEntities{
     
+    public TheOneRing(String id) {
+        this.id = id;
+        this.type = "the_one_ring";
+        this.dropRate = 1;
+    }
 }
