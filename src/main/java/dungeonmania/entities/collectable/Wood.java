c7432d437b54d4e9c56615fad8ef0ee852f2c733
Austin Chen <yizhou.chen2@student.unsw.edu.au>
2021-10-29 01:31:15 +1100

    manually copying codes due to merge conflict

--- src/main/java/dungeonmania/entities/collectable/Wood.java
+++ src/main/java/dungeonmania/entities/collectable/Wood.java
@@ -1,9 +1,11 @@
 package dungeonmania.entities.collectable;
 
+import dungeonmania.util.Position;
+
 public class Wood extends CollectableEntity {
 
-    public Wood(String id) {
-        this.id = id;
-        this.type = "wood";
+    public Wood(Position position) {
+        super("wood", position, false);
     }
 }
+
