5b8178edfbf900b571af83c884c35469720d1c01
Austin Chen <yizhou.chen2@student.unsw.edu.au>
2021-10-28 18:10:35 +1100

    Fixed input parameter

--- src/main/java/dungeonmania/entities/collectable/Wood.java
+++ src/main/java/dungeonmania/entities/collectable/Wood.java
@@ -2,7 +2,7 @@ package dungeonmania.entities.collectable;
 
 public class Wood extends CollectableEntity {
 
-    public Wood(String id, String type) {
+    public Wood(String id) {
         this.id = id;
         this.type = "wood";
     }
