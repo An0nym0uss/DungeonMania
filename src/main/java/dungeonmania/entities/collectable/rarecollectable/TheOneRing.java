8903466017eca3c3262dcba7498bf403c1044453
Austin Chen <yizhou.chen2@student.unsw.edu.au>
2021-10-29 01:40:16 +1100

    added method

--- src/main/java/dungeonmania/entities/collectable/rarecollectable/TheOneRing.java
+++ src/main/java/dungeonmania/entities/collectable/rarecollectable/TheOneRing.java
@@ -1,10 +1,19 @@
 package dungeonmania.entities.collectable.rarecollectable;
 
+import dungeonmania.entities.player.StatusEffect;
+import dungeonmania.entities.player.Player;
+
 public class TheOneRing extends RareCollectableEntities{
     
     public TheOneRing(String id) {
         this.id = id;
         this.type = "the_one_ring";
         this.dropRate = 1;
+    }
+
+    public void respawn(Player player, StatusEffect statusEffect) {
+        player.setCurrentHealth(player.getMaxHealth());
+        statusEffect.setInvincible(true);
+        statusEffect.setInvincibleDuration(3);
     }
 }
