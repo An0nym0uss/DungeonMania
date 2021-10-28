baefaed3739345f0027d126f09ad139f2a29aa86
Hoy Wang <hoy.wang@student.unsw.edu.au>
2021-10-28 21:15:41 +1100

    partially implemented player

--- src/main/java/dungeonmania/entities/player/ReadRecipe.java
+++ src/main/java/dungeonmania/entities/player/ReadRecipe.java
@@ -19,6 +19,9 @@ public class ReadRecipe {
         try {
             String read = FileLoader.loadResourceFile(filename);
             JSONObject js = new JSONObject(read);
+            // get type of buildable
+            Object obj = js.get("type");
+            String type = (String) obj;
             // each info is a piece of recipe
             JSONArray recipes = (JSONArray) js.get("ingredients");
             for (int i = 0; i < recipes.length(); i++) {
@@ -33,14 +36,14 @@ public class ReadRecipe {
                 for (String single : list) {
                     List<String> ingredient = new ArrayList<>();
                     ingredient = Arrays.asList(single.split(":"));
-                    String type = ingredient.get(0);
-                    type = type.substring(1, type.length() - 1);
+                    String ingredientType = ingredient.get(0);
+                    ingredientType = ingredientType.substring(1, ingredientType.length() - 1);
                     int amount = Integer.parseInt(ingredient.get(1));
 
-                    ingredients.put(type, amount);
+                    ingredients.put(ingredientType, amount);
                 }
 
-                Recipe recipe = new Recipe(ingredients);
+                Recipe recipe = new Recipe(type, ingredients);
                 recipeCollection.add(recipe);
             }
             
@@ -53,10 +56,11 @@ public class ReadRecipe {
     }
     public static void main(String[] args) {
         ReadRecipe read = new ReadRecipe();
-        String filename = "/dungeonmania/entities/collectable/buildable/recipes/shieldRecipe.json";
+        String filename = "/dungeonmania/entities/collectable/buildable/recipes/bowRecipe.json";
         
         for (Recipe recipe : read.readRecipes(filename)) {
             System.out.println("------------------");
+            System.out.println(recipe.getType() + ": ");
             for (HashMap.Entry<String, Integer> ingredient : recipe.getIngredients().entrySet()) {
                 System.out.println(ingredient.getKey() + " " + ingredient.getValue());
             }
