e153719269f9529ffb299e58d29c4b93cf932850
Austin Chen <yizhou.chen2@student.unsw.edu.au>
2021-10-28 18:11:14 +1100

    Fixed input parameter

--- src/main/java/dungeonmania/entities/collectable/Key.java
+++ src/main/java/dungeonmania/entities/collectable/Key.java
@@ -2,7 +2,7 @@ package dungeonmania.entities.collectable;
 
 public class Key extends CollectableEntity{
 
-    public Key(String id, String type) {
+    public Key(String id) {
         this.id = id;
         this.type = "key";
     }
