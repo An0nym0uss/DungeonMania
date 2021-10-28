1bb852f2ecc8148176ca49b018408da6bcad13fd
Austin Chen <yizhou.chen2@student.unsw.edu.au>
2021-10-28 18:06:24 +1100

    Update RareCollectableEntities.java

--- src/main/java/dungeonmania/entities/collectable/rarecollectable/RareCollectableEntities.java
+++ src/main/java/dungeonmania/entities/collectable/rarecollectable/RareCollectableEntities.java
@@ -1,8 +1,28 @@
 package dungeonmania.entities.collectable.rarecollectable;
 
 import dungeonmania.entities.collectable.CollectableEntity;
+import dungeonmania.entities.player.Inventory;
+
+import java.util.Random;
+
 
 public abstract class RareCollectableEntities extends CollectableEntity {
-    private double dropDate;
-    
+
+    protected double dropRate;
+
+    public void spawnnRareCollectableEntities(Inventory inventory) {
+        Random random = new Random();
+        int draw = random.nextInt(100);
+        if (draw + 1 <= this.dropRate) {
+            inventory.addItem(this);
+        }
+    }
+
+    public double getDropRate() {
+        return this.dropRate;
+    }
+
+    public void setDropRate(double dropRate) {
+        this.dropRate = dropRate;
+    }
 }
