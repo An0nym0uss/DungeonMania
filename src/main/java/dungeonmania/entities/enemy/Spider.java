2e01d4d640e20497ed9185e655f5160c6d6170a8
Lachlan Kerr <lachlankerr96@gmail.com>
2021-11-01 11:22:52 +1100

    Fix spider, still 85% coverage

--- src/main/java/dungeonmania/entities/enemy/Spider.java
+++ src/main/java/dungeonmania/entities/enemy/Spider.java
@@ -1,7 +1,9 @@
 package dungeonmania.entities.enemy;
 
 import dungeonmania.Grid;
+import dungeonmania.entities.Battle;
 import dungeonmania.entities.Entity;
+import dungeonmania.entities.player.Player;
 import dungeonmania.entities.statics.Boulder;
 import dungeonmania.util.Direction;
 import dungeonmania.util.Position;
@@ -10,18 +12,15 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import java.util.*;
-
 public class Spider extends Enemy {
-    private List<Direction> movesUpWhenSpawns = new ArrayList<Direction>();
     private List<Direction> movementArray = new ArrayList<Direction>();
     private int directionCount;
     private boolean isReverse;
+    private int maxSpiders = 4;
 
     public Spider(Position position,int speed, int health, int damage){
         super("spider", position, false, speed, health, damage);
-        this.movesUpWhenSpawns.addAll(Arrays.asList(Direction.UP));
-        this.movementArray.addAll(Arrays.asList(Direction.RIGHT,
+        this.movementArray.addAll(Arrays.asList(Direction.UP, Direction.RIGHT,
                 Direction.DOWN, Direction.DOWN,
                 Direction.LEFT, Direction.LEFT,
                 Direction.UP,  Direction.UP,
@@ -32,24 +31,7 @@ public class Spider extends Enemy {
 
     @Override
     public void update(Grid grid) {
-        //spawns
-        Direction upDirection = movesUpWhenSpawns.get(directionCount);
-        // tries to move to next position
-        Direction nextDirection = movementArray.get(directionCount);
-        Position newPosition = this.getPosition().translateBy(isReverse ? Direction.getOppositeDirection(nextDirection) : nextDirection);
-        // if can't move there, reverse direction
-        // TODO need to add grid constraint too
-        List<Entity> positionEntities = grid.getEntities(newPosition.getX(), newPosition.getY());
-        for (Entity positionEntity : positionEntities) {
-            if (this.movingConstraints(positionEntity)) {
-                isReverse = !isReverse;
-                return;
-            }
-        }
-        // move to new position
-        // probably should use move()
-        this.setPosition(newPosition);
-
+        move(grid, movementArray.get(directionCount));
     }
 
     @Override
@@ -69,12 +51,76 @@ public class Spider extends Enemy {
 
     @Override
     public void move(Grid grid, Direction d) {
+        // tries to move to next position
+        Position newPosition = this.getPosition().translateBy(isReverse ? Direction.getOppositeDirection(d) : d);
+        
+        int x = newPosition.getX();
+        int y = newPosition.getY();
+        if (x >= 0 && x < grid.getWidth() &&
+            y >= 0 && y < grid.getHeight()
+        ) { // check grid boundary
+            List<Entity> positionEntities = grid.getEntities(newPosition.getX(), newPosition.getY());
+            for (Entity positionEntity : positionEntities) {
+                if (positionEntity instanceof Player) { // do a battle with player
+                    Battle.battle((Player)positionEntity, (Enemy)this, grid);
+                }
+                else if (movingConstraints(positionEntity)) { // if can't move there, reverse direction
+                    if (directionCount == 0) { // boulder in first position, do nothing till removed
+                        return;
+                    }
+                    newPosition = reversePosition();
+                }
+            }
+        } 
+        else { // if can't move there, reverse direction
+            if (directionCount == 0) { // spider at top of boundary, do nothing
+                return;
+            }
+            newPosition = reversePosition();
+        }
+        // move to new positiond
+        setPosition(newPosition);
 
+        nextDirection();
+    }
+
+    /**
+     * Increments or decremenets the direction counter.
+     */
+    private void nextDirection() {
+        if (!isReverse) {
+            if (directionCount == 8) {
+                directionCount = 1;
+            }
+            else {
+                directionCount++;
+            }
+        }
+        else {
+            if (directionCount == 1) {
+                directionCount = 8;
+            }
+            else {
+                directionCount--;
+            }
+        }
+    }
+
+    /**
+     * Reverses the spiders position.
+     * Call if we hit a boulder or grid boundary.
+     * @return The new position to move to.
+     */
+    private Position reversePosition() {
+        isReverse = !isReverse;
+        nextDirection();
+        Direction d = movementArray.get(directionCount);
+        return getPosition().translateBy(isReverse ? Direction.getOppositeDirection(d) : d);
     }
 
     @Override
     public boolean movingConstraints(Entity e) {
-    if (e instanceof Boulder) {
+        if (e instanceof Boulder) {
             return true;
         }
         return false;
--- src/test/java/dungeonmania/WallTest.java
+++ src/test/java/dungeonmania/WallTest.java
@@ -1,5 +1,6 @@
 package dungeonmania;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -17,15 +18,18 @@ public class WallTest {
      */
     public void testWallBasic() {
         DungeonManiaController controller = new DungeonManiaController();
+        // Changing where the files are loaded from.
+        StandardDungeonMaker.RESOURCE_PATH = "src/test/resources/dungeons/";
 
         // start game
-        controller.newGame("wall", "peaceful");
+        controller.newGame("wall", "standard");
 
-        // attempt to walk into wall
+        // attempt to kill first spider
         DungeonResponse firstTick = controller.tick(null, Direction.RIGHT);
 
         int playerId = 0;
         int spiderId = 0;
+        int spiderCount = 0;
 
         // get playerId and spider
         for( int i = 0; i < firstTick.getEntities().size(); i++) {
@@ -34,19 +38,78 @@ public class WallTest {
                 playerId = i;
             } else if (entResponse.getType().equals("spider")) {
                 spiderId = i;
+                spiderCount++;
+            }
+        }
+
+        // only 1 spider left
+        assertEquals(spiderCount, 1);
+
+        // wall impassable
+        //assertTrue(firstTick.getEntities().get(playerId).getType().equals("player") && firstTick.getEntities().get(playerId).getPosition().getX() == 0);
+
+        // spider can move onto wall
+        assertTrue(firstTick.getEntities().get(spiderId).getType().equals("spider") && firstTick.getEntities().get(spiderId).getPosition().getX() == 4 && firstTick.getEntities().get(spiderId).getPosition().getY() == 0);
+
+        // attempt spider reverse direction
+        DungeonResponse secondTick = controller.tick(null, Direction.DOWN);
+
+        // get playerId and spider
+        for( int i = 0; i < secondTick.getEntities().size(); i++) {
+            EntityResponse entResponse = secondTick.getEntities().get(i);
+            if (entResponse.getType().equals("player")) {
+                playerId = i;
+            } else if (entResponse.getType().equals("spider")) {
+                spiderId = i;
+            }
+        }
+
+        // spider reversed direction
+        assertTrue(secondTick.getEntities().get(spiderId).getType().equals("spider") && secondTick.getEntities().get(spiderId).getPosition().getX() == 3 && secondTick.getEntities().get(spiderId).getPosition().getY() == 0);
+
+        controller.tick(null, Direction.RIGHT);
+        controller.tick(null, Direction.RIGHT);
+        controller.tick(null, Direction.UP);
+        DungeonResponse sixthTick = controller.tick(null, Direction.RIGHT);
+
+        // get playerId and spider
+        for( int i = 0; i < sixthTick.getEntities().size(); i++) {
+            EntityResponse entResponse = sixthTick.getEntities().get(i);
+            if (entResponse.getType().equals("player")) {
+                playerId = i;
+            } else if (entResponse.getType().equals("spider")) {
+                spiderId = i;
             }
         }
 
         // wall impassable
-        assertTrue(firstTick.getEntities().get(playerId).getType().equals("player") && firstTick.getEntities().get(playerId).getPosition().getX() == 0);
+        assertTrue(sixthTick.getEntities().get(playerId).getType().equals("player") && sixthTick.getEntities().get(playerId).getPosition().getX() == 3 && sixthTick.getEntities().get(playerId).getPosition().getY() == 0);
 
-        // spider can move onto wall
-        assertTrue(firstTick.getEntities().get(spiderId).getType().equals("spider") && firstTick.getEntities().get(spiderId).getPosition().getX() == 1 && firstTick.getEntities().get(spiderId).getPosition().getY() == 0);
+        controller.tick(null, Direction.DOWN);
+        DungeonResponse eighthTick = controller.tick(null, Direction.LEFT);
+
+        // get playerId and spider
+        for( int i = 0; i < eighthTick.getEntities().size(); i++) {
+            EntityResponse entResponse = eighthTick.getEntities().get(i);
+            if (entResponse.getType().equals("player")) {
+                playerId = i;
+            } else if (entResponse.getType().equals("spider")) {
+                spiderId = i;
+            }
+        }
+
+        // spider reversed direction again
+        assertTrue(eighthTick.getEntities().get(spiderId).getType().equals("spider") && eighthTick.getEntities().get(spiderId).getPosition().getX() == 5 && eighthTick.getEntities().get(spiderId).getPosition().getY() == 2);
+
+        controller.tick(null, Direction.LEFT);
 
         // finish game
-        controller.tick(null, Direction.DOWN);
+        assertEquals("", controller.tick(null, Direction.LEFT).getGoals());
 
-        (new Position(1, 2)).getAdjacentPositions();
+        // finish game
+        //assertEquals("", controller.tick(null, Direction.DOWN).getGoals());
+
+        //(new Position(1, 2)).getAdjacentPositions();
     }
 
     @Test
