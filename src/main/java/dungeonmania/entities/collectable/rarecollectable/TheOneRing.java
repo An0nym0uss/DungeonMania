8ea8ca3e3a1c1306f5cce6bf68a3c9c8a9a465ff
Austin Chen <yizhou.chen2@student.unsw.edu.au>
2021-11-08 08:12:05 +1100

    Made refactoring changes

--- src/main/java/dungeonmania/entities/collectable/rarecollectable/TheOneRing.java
+++ src/main/java/dungeonmania/entities/collectable/rarecollectable/TheOneRing.java
@@ -1,11 +1,28 @@
 package dungeonmania.entities.collectable.rarecollectable;
 
 import dungeonmania.util.Position;
+import dungeonmania.entities.player.Inventory;
+import dungeonmania.entities.collectable.CollectableEntity;
 
 public class TheOneRing extends RareCollectableEntities{
     
-    public TheOneRing(Position position) {
-        super("the_one_ring", position, false);
-        setDropRate(1);
+    public TheOneRing() {
+        super("the_one_ring", new Position(0,0), false, 5);
+    }
+
+    @Override
+    public void spawnnOneRing(Inventory inventory) {
+        boolean hasRareCollectable = false;
+        // can only have one one ring
+        for (CollectableEntity entity : inventory.getItems()) {
+            if (entity instanceof TheOneRing) {
+                hasRareCollectable = true;
+            }
+        }
+        if (!hasRareCollectable) {
+            if (random.nextInt(100) < 5) {
+                inventory.addItem(this);
+            }
+        }
     }
 }
