4ac2fa7a4cfd9005b9da32b455c7920f70e976f3
Austin Chen <yizhou.chen2@student.unsw.edu.au>
2021-11-09 00:45:48 +1100

    Fixed bug

--- src/main/java/dungeonmania/entities/Interaction.java
+++ src/main/java/dungeonmania/entities/Interaction.java
@@ -13,6 +13,7 @@ import dungeonmania.util.Position;
 
 public class Interaction {
     public static void interactMerc (Mercenary entity, Grid grid, Player player) {
+        boolean success = false;
         Position position = player.getPosition();
         for (Position adjacentPosition : getPosition(position)) {
             for (Entity adjacentEntity : grid.getEntities(adjacentPosition.getX(), adjacentPosition.getY())) {
@@ -20,28 +21,37 @@ public class Interaction {
                     if (!player.hasTreasure()) {
                         throw new InvalidActionException("You do not have any gold");
                     } else {
-                        entity.setBribed(true); 
+                        entity.setBribed(true);
+                        player.getInventory().removeNonSpecificItem("trerasure"); 
+                        success = true;
                     }
                 }
             }
         }
-        throw new InvalidActionException("You not within 2 cardinal tiles to the mercenary");
+        if (!success) {
+            throw new InvalidActionException("You not within 2 cardinal tiles to the mercenary");
+        }
     }
     
     public static void interactSpawner(ZombieToastSpawner entity, Grid grid, Player player) {
+        boolean success = false;
         Position position = player.getPosition();
         for (Position adjacentPosition : position.getAdjacentCardinalPositions()) {
             for (Entity adjacentEntity : grid.getEntities(adjacentPosition.getX(), adjacentPosition.getY())) {
                 if (adjacentEntity.getType() == entity.getType()) {
+                    System.out.println("lala");
                     if (!player.hasSword()) {
                         throw new InvalidActionException("You do not have a weapon");
                     } else {
                         grid.dettach(entity);
+                        success = true;
                     }
                 } 
             }
         }
-        throw new InvalidActionException("You are not cardinally adjacent to the spawner");
+        if (!success) {
+            throw new InvalidActionException("You are not cardinally adjacent to the spawner");
+        }
     }
     // return all position within 2 cardianl tiles of the given position, used
     // for finding mercenary within range
