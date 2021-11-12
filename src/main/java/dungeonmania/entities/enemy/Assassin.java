a7dfaf36749142a02905530252322e79a5d86546
William Wong <z5205428@ad.unsw.edu.au>
2021-11-12 12:44:55 +1100

    Need to fix the constructor of Assassin

--- src/main/java/dungeonmania/entities/enemy/Assassin.java
+++ src/main/java/dungeonmania/entities/enemy/Assassin.java
@@ -1,26 +1,30 @@
 package dungeonmania.entities.enemy;
 
-import dungeonmania.entities.collectable.CollectableEntity;
-import org.json.JSONObject;
-
 import dungeonmania.Grid;
 import dungeonmania.entities.Entity;
-import dungeonmania.entities.statics.Boulder;
-import dungeonmania.entities.statics.Door;
-import dungeonmania.entities.statics.Wall;
 import dungeonmania.util.Direction;
-import dungeonmania.entities.player.Player;
 import dungeonmania.util.Position;
-//import dungeonmania.util.Mercenary;
+import dungeonmania.entities.enemy.Mercenary;
 
 import java.util.List;
 import java.util.*;
 
-public class Assassin extends Enemy {
+public class Assassin extends Mercenary {
+    private Mercenary mercenary;
 
-    public Assassin(String type, Position position, Boolean isInteractable, int speed, int health, int damage) {
+    public Assassin(Position position, int speed, int health, int damage) {
         super("assassin", position, false, speed, health, damage);
     }
+
+    @Override
+    public void update(Grid grid) {
+        mercenary.update(grid);
+    }
+
+    public List<Position> breadthFirstSearch(Grid grid) {
+        return mercenary.breadthFirstSearch(grid);
+    }
+
 
     @Override
     public void move(Grid grid, Direction d) {
@@ -30,7 +34,6 @@ public class Assassin extends Enemy {
 
     @Override
     public boolean movingConstraints(Entity e) {
-        // TODO Auto-generated method stub
-        return false;
+        return mercenary.movingConstraints(e);
     }
 }
