ec1caa78a4e9a8e08861e707b14b0f68a9a733ac
Lachlan Kerr <lachlankerr96@gmail.com>
2021-11-12 21:19:15 +1100

    Add isAffectedBy method to swamp tile

--- src/main/java/dungeonmania/entities/statics/SwampTile.java
+++ src/main/java/dungeonmania/entities/statics/SwampTile.java
@@ -1,7 +1,14 @@
 package dungeonmania.entities.statics;
 
+import dungeonmania.entities.Entity;
+import dungeonmania.entities.enemy.Enemy;
 import dungeonmania.util.Position;
 
+/**
+ * Swamp tiles slow down the movement of enemy entities by the provided movement factor.
+ * i.e. it takes 'movementFactor' ticks to traverse the tile.
+ * @author Lachlan Kerr
+ */
 public class SwampTile extends StaticEntity {
 
     private int movementFactor;
@@ -10,8 +17,23 @@ public class SwampTile extends StaticEntity {
         super("swamp_tile", position, false);
         this.movementFactor = movementFactor;
     }
-
+    
+    /**
+     * @return The movement factor 
+     */
     public int getMovementFactor() {
         return movementFactor;
+    }
+    
+    /**
+     * Checks whether the given entity should be affected by the swamp tile.
+     * @param entity The entity we wish to check.
+     * @return True if swamp tiles affects entity, otherwise false.
+     */
+    public boolean isAffectedBy(Entity entity) {
+        if (entity instanceof Enemy) {
+            return true;
+        }
+        return false;
     }
 }
