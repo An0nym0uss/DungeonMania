612504dd8d802643e58340c2ccd843a51ae138a5
Austin Chen <yizhou.chen2@student.unsw.edu.au>
2021-11-14 21:28:03 +1100

    Added tests

--- src/test/java/dungeonmania/InteractTest.java
+++ src/test/java/dungeonmania/InteractTest.java
@@ -2,21 +2,21 @@ package dungeonmania;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
+
 
 import dungeonmania.response.models.DungeonResponse;
 import dungeonmania.util.Direction;
 import dungeonmania.util.Position;
 import dungeonmania.entities.player.Player;
-import dungeonmania.entities.collectable.Sword;
+import dungeonmania.entities.collectable.*;
+import dungeonmania.entities.collectable.rarecollectable.*;
 import dungeonmania.entities.enemy.Enemy;
 import dungeonmania.entities.enemy.Mercenary;
+import dungeonmania.entities.enemy.Assassin;
+import dungeonmania.entities.enemy.Spider;
 import dungeonmania.entities.statics.ZombieToastSpawner;
 import dungeonmania.exceptions.InvalidActionException;
-import dungeonmania.entities.collectable.*;
 import dungeonmania.entities.Entity;
 
 import dungeonmania.constants.Layer;
@@ -59,7 +59,122 @@ public class InteractTest {
         assertTrue(hasTreasure.getInventory().size() == 1);
         DungeonResponse nextToMercenary = controller.interact(hasTreasure.getEntities().get(1).getId());
         assertTrue(nextToMercenary.getInventory().size() == 0);
+    }
 
+    @Test
+    public void testMindControl() {
+
+        DungeonManiaController controller = new DungeonManiaController();
+        StandardDungeonMaker.RESOURCE_PATH = "src/test/resources/dungeons/";
+        controller.newGame("mindControl", "standard");
+
+        controller.tick(null, Direction.RIGHT);
+        controller.tick(null, Direction.RIGHT);
+        DungeonResponse canBuild = controller.tick(null, Direction.RIGHT);
+        assertTrue(canBuild.getInventory().size() == 3);
+        DungeonResponse buildSceptre = controller.build("sceptre");
+        assertTrue(buildSceptre.getInventory().size() == 1);
+        assertTrue(buildSceptre.getEntities().get(0).getType() == "player");
+        assertTrue(buildSceptre.getEntities().get(0).getPosition().getX() == 4);
+        assertTrue(buildSceptre.getEntities().get(1).getType() == "mercenary");
+        assertTrue(buildSceptre.getEntities().get(1).getPosition().getX() == 5);
+        assertDoesNotThrow(() -> controller.interact(buildSceptre.getEntities().get(1).getId()));
+    }
+
+    @Test
+    public void testMercBattle() {
+
+        Player player = new Player(new Position(5, 5, Layer.PLAYER), new Standard());
+        CollectableEntity treasure = new Treasure(new Position(6, 5, Layer.COLLECTABLE));
+        Enemy merc = new Mercenary(new Position(7, 5), 1, 50, 2);
+        Enemy enemy = new Spider(new Position(6, 6), 1, 100, 1);
+        Grid grid = new Grid(10, 10, new Entity[10][10][Layer.LAYER_SIZE], null);
+        grid.attach(player);
+        grid.attach(merc);
+        grid.attach(enemy);
+        grid.attach(treasure);
+
+        assertTrue(player.getInventory().getItems().isEmpty());
+        player.move(grid, Direction.RIGHT);
+        assertTrue(player.getInventory().getItems().size() == 1);
+        player.interact(merc.getId(), grid);
+        assertTrue(player.getInventory().getItems().isEmpty());
+        player.move(grid, Direction.DOWN);
+        assertTrue(grid.getEntities(6, 6).size() == 1);
+        assertTrue(grid.getEntities(6, 6).get(0).getType() == "player");
+    }
+
+    @Test
+    public void testBribeAssassin() {
+
+        Player player = new Player(new Position(5, 5, Layer.PLAYER), new Standard());
+        CollectableEntity treasure = new Treasure(new Position(6, 5, Layer.COLLECTABLE));
+        Enemy enemy = new Assassin(new Position(7, 5), 1, 1, 1);
+        Grid grid = new Grid(10, 10, new Entity[10][10][Layer.LAYER_SIZE], null);
+        grid.attach(player);
+        grid.attach(enemy);
+        grid.attach(treasure);
+
+        assertTrue(player.getInventory().getItems().isEmpty());
+        player.move(grid, Direction.RIGHT);
+        assertTrue(player.getInventory().getItems().size() == 1);
+        // spawn a one ring
+        RareCollectableEntities ring = new TheOneRing();
+        ring.setDropRate(100);
+        ring.spawnnOneRing(player.getInventory());
+
+        assertTrue(player.getInventory().getItems().size() == 2);
+        player.interact(enemy.getId(), grid);
+        assertTrue(player.getInventory().getItems().isEmpty());
+    }
+
+    @Test
+    public void testBribeNotAdjancent() {
+
+        Player player = new Player(new Position(1, 1, Layer.PLAYER), new Standard());
+        CollectableEntity treasure = new Treasure(new Position(2, 1, Layer.COLLECTABLE));
+        Enemy enemy = new Mercenary(new Position(5, 5), 1, 1, 1);
+        Grid grid = new Grid(10, 10, new Entity[10][10][Layer.LAYER_SIZE], null);
+        grid.attach(player);
+        grid.attach(enemy);
+        grid.attach(treasure);
+
+        assertTrue(player.getInventory().getItems().isEmpty());
+        player.move(grid, Direction.RIGHT);
+        assertTrue(player.getInventory().getItems().size() == 1);
+        assertThrows(InvalidActionException.class, () -> player.interact(enemy.getId(), grid));
+    }
+
+    @Test
+    public void testBribeNoTreasure() {
+
+        Player player = new Player(new Position(1, 1, Layer.PLAYER), new Standard());
+        Enemy enemy = new Mercenary(new Position(5, 5), 1, 1, 1);
+        Grid grid = new Grid(10, 10, new Entity[10][10][Layer.LAYER_SIZE], null);
+        grid.attach(player);
+        grid.attach(enemy);
+
+        assertTrue(player.getInventory().getItems().isEmpty());
+        player.move(grid, Direction.RIGHT);
+        assertTrue(player.getInventory().getItems().size() == 0);
+        assertThrows(InvalidActionException.class, () -> player.interact(enemy.getId(), grid));
+    }
+
+    @Test
+    public void testBribeNoOneRing() {
+
+        Player player = new Player(new Position(5, 5, Layer.PLAYER), new Standard());
+        CollectableEntity treasure = new Treasure(new Position(6, 5, Layer.COLLECTABLE));
+        Enemy enemy = new Assassin(new Position(7, 5), 1, 1, 1);
+        Grid grid = new Grid(10, 10, new Entity[10][10][Layer.LAYER_SIZE], null);
+        grid.attach(player);
+        grid.attach(enemy);
+        grid.attach(treasure);
+
+        assertTrue(player.getInventory().getItems().isEmpty());
+        player.move(grid, Direction.RIGHT);
+        assertTrue(player.getInventory().getItems().size() == 1);
+        assertThrows(InvalidActionException.class, () -> player.interact(enemy.getId(), grid));
     }
 
     @Test
